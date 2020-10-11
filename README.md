3f1ec65f2da0834b891e9b5339af1d874ecfe2c0
abdallahshanaah <ayousef18@uwcchina.org>
2020-10-11 15:54:18 +0300

    day 1

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # notesy
 
 day1: Make the files and directories. Make the first two constructors of input and notes.
+
+
+![Day-1-UIM](./UIM/day-1.PNG)
--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 'use strict';
 
+// This is the main entry point 
+// Requires libraries from folder lib
+// Make an instance of Input then sends it to Note
+
 const Input = require('./lib/input.js');
 const Notes = require('./lib/notes.js');
 
